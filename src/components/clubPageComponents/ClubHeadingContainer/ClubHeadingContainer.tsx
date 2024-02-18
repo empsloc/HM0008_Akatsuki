8f2867ff072af5eabcafc77417660d16bced4d06
empsloc <empsloc@gmail.com>
2024-02-18 12:26:50 +0530

    adsd

--- src/components/clubPageComponents/ClubHeadingContainer/ClubHeadingContainer.tsx
+++ src/components/clubPageComponents/ClubHeadingContainer/ClubHeadingContainer.tsx
@@ -5,7 +5,7 @@ const ClubHeadingContainer=()=>{
         <div className="">
             <div className="grid grid-cols-2">
                 <div className="flex justify-center items-center"><div className="text-5xl font-bold">Team CESA</div></div>
-                <div className="md:pl-16"><div className="w-full h-96 relative"><Image src="/com2.png" alt="" fill/></div></div>
+                <div className="md:pl-16"><div className="w-full h-96 relative"><Image src="/coms1.png" alt="" fill/></div></div>
             </div>
         </div>
     )
